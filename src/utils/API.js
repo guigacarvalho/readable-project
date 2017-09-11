1a04cdfe44831a2c8c860eccac8e674c7218ec40
Guilherme Carvalho <guilherme.carvalho@gmail.com>
2017-09-11 13:00:48 -0700

    Adding an API header

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,7 +1,8 @@
 
-export const rootUrl = "http://localhost:5001"
+export const rootUrl = "http://localhost:3001"
 
 export const headers = {
   'Accept': 'application/json',
+  'Content-Type': 'application/json',
   'Authorization': '???',
 }
