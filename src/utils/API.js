a46a74555444661a558dd600aad5408b37593272
Guilherme B. Carvalho <guilherme.carvalho@gmail.com>
2017-10-07 15:46:38 -0700

    Allowing support for users that don't use env variables

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,5 +1,5 @@
 // REACT_APP_ROOT_URL defined in .env.development 
-export const rootUrl = process.env.REACT_APP_ROOT_URL
+export const rootUrl = process.env.REACT_APP_ROOT_URL  || `http://localhost:3001`
 
 export const headers = {
   'Accept': 'application/json',
