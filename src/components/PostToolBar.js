a0fe4bb27ed0b987e544ebb3544df98643882a1e
Guilherme Carvalho <guilherme_carvalho@homedepot.com>
2017-10-23 11:45:48 -0700

    Fixing comment count bug on post listings

--- src/components/PostToolBar.js
+++ src/components/PostToolBar.js
@@ -23,11 +23,10 @@ class PostToolBar extends React.Component {
   render(){
     const {post} = this.props
     const [updatedPost] = this.props.posts.filter((postFromState)=> postFromState.id === post.id) 
-    const commentsLength = this.props.comments.filter((commentFromState)=> commentFromState.parentId === updatedPost.id).length;
     return (
       <div>
         <button className="button button-clear button-small controls"># {updatedPost.voteScore}</button> |
-        <button className="button button-clear button-small controls">{ emoji('💬') } { commentsLength }</button> |
+        <button className="button button-clear button-small controls">{ emoji('💬') } { updatedPost.commentCount }</button> |
         <button className="button button-clear button-small controls"><span role="img" aria-label="upvote" onClick={()=>this.upVote(updatedPost.id)}>{ emoji('👍') }</span></button> |
         <button className="button button-clear button-small controls"><span role="img" aria-label="downvote" onClick={()=>this.downVote(updatedPost.id)}>{ emoji('👎') }</span></button> |
         <button className="button button-clear button-small controls"><span role="img" aria-label="edit" onClick={()=>this.editPost(updatedPost.id)}>{ emoji('📝') }</span></button> |
@@ -39,7 +38,6 @@ class PostToolBar extends React.Component {
 
 const mapStateToProps = (state, props) => ({
   posts: state.postsReducer.posts,
-  comments: state.commentsReducer.comments,
 });
 
 
